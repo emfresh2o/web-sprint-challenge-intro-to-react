1dd9c49a27d9ccbd83888f081ffa8985cefa6120
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-08-04 01:31:01 -0400

    minor changes

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Character from './components/Character';
+import styled from 'styled-components'
+
+const Header = styled.h1`
+  color: lightgreen;
+  text-align: center;
+  font-size: 50px;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,6 +20,8 @@ const handleChange = e => {
 //console.log(searchWord);
 }
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about whch state and/or props it should
+  // sync up with, if any
 useEffect(() => {
   axios
     .get('https://rickandmortyapi.com/api/character')
@@ -28,7 +37,7 @@ useEffect(() => {
 //console.log(data);
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Header>Characters</Header>
       <label>
         Search character here:
         <input type='text' name='search' value={searchWord} onChange={handleChange}/>
